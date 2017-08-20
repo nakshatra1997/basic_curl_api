75c9421f98f861bdb38f8cd6f48ad4919daf79d3
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-08-20 08:28:22 +0530

    sending data to server and getting tesponse from it

--- data.php
+++ data.php
@@ -1,4 +1,18 @@
 <?php
 //whatever the server {echo },it will be the response for client
-echo "hello client";
+// echo "hello client";
+$myVar=$_POST['myVar'];
+//connecting with database
+$conn=mysqli_connect("localhost","root","","tasks") or die("error");
+//executing the query
+
+$result=mysqli_query($conn,'SELECT * FROM account WHERE account_id ='.$myVar)or die("error") ;
+//an array to record data
+$data=array();
+while($row=mysqli_fetch_assoc($result))
+{
+	$data[]=$row;
+}
+//returning the array to client
+echo json_encode($data);
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php
 $curl=curl_init(); //starting the curl session and storing its instance
+//set the variable u want to send in curl
+$var=2;
 //setting the url of the server
 curl_setopt($curl, CURLOPT_URL, "http://localhost/api/data.php");
+//setting the variable in CURL option
+curl_setopt($curl, CURLOPT_POSTFIELDS, "myVar=".$var);
+//removing 1 from server response
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
 //executing the request and storing server's response in variable
 $response=curl_exec($curl);
-echo "response is". $response;
+//store the server response,call it data
+$data=json_decode($response);
+// print_r($data);
+//displaying the data all attributes
+
+for($a=0;$a<count($data);$a++)
+{
+	echo $data[$a]->account_id.",";
+	echo $data[$a]->email.",";
+	echo $data[$a]->password.",";
+	echo "<br/>";
+}
 ?>
